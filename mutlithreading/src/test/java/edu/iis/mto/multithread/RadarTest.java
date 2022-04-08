89a618b2412768226f6142071d1f77633451dee7
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-04-08 18:45:05 +0200

    kilka testów na różną ilość rakiet

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,6 +21,34 @@ class RadarTest {
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
         verify(batteryMock).launchPatriot(enemyMissle);
+    }
+
+    @RepeatedTest(20)
+    void launchPatriotThreeTimesWhenNoticesAScudMissle() {
+        int rocket_count = 3;
+        BetterRadar radar = new BetterRadar(batteryMock, rocket_count);
+        Scud enemyMissle = new Scud();
+        radar.notice(enemyMissle);
+        verify(batteryMock, Mockito.times(rocket_count)).launchPatriot(enemyMissle);
+    }
+
+    @RepeatedTest(30)
+    void launchPatriotThreeTimesWhenNoticesAScudEnemyMissles() {
+        int rocket_count = 3; int enemy_rockets = 2;
+        BetterRadar radar = new BetterRadar(batteryMock, rocket_count);
+        Scud enemyMissle = new Scud();
+        for(int i = 0; i < enemy_rockets; i++){
+            radar.notice(enemyMissle);
+        }
+        verify(batteryMock, Mockito.times(rocket_count * enemy_rockets)).launchPatriot(enemyMissle);
+    }
+
+    @Test
+    void weAreNotAtWorkRightNow() {
+        int rocket_count = 3; int enemy_rockets = 0;
+        BetterRadar radar = new BetterRadar(batteryMock, rocket_count);
+        Scud enemyMissle = new Scud();
+        verify(batteryMock, Mockito.times(0)).launchPatriot(enemyMissle);
     }
 
 }
