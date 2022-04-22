e817c67c364eb8dd7219a602baea37c2301021ba
ImBioMario <m.sukiennik95@gmail.com>
2022-04-22 16:35:17 +0200

    Added BetterRadar and modified a test.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,24 +1,33 @@
 package edu.iis.mto.multithread;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
 
     @Mock
     private PatriotBattery batteryMock;
+    @Mock
+    ExecutorService executor;
 
-    @Test
+    @RepeatedTest(100)
+    //@Test
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+        BetterRadar radar = new BetterRadar(10, batteryMock, executor);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        verify(executor).execute(any(Runnable.class));
     }
-
 }
